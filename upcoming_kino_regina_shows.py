21b9f45bb03a39f9ec688fc0a3e7e1fbff8cd832
Ville Touronen <ville.touronen@reaktor.com>
2020-10-02 22:23:55 +0300

    Update movie endpoint

--- upcoming_kino_regina_shows.py
+++ upcoming_kino_regina_shows.py
@@ -23,7 +23,7 @@ printKinoReginaArt()
 now = datetime.now().strftime('%d-%m-%Y')
 
 post_params = urllib.parse.urlencode({'getShowtimesFrontpage': now}).encode("utf-8")
-post_request = urllib.request.Request("http://kinoregina.fi/wp-content/themes/kinoregina/assets/functions/getShowtimesFrontpage.php", data=post_params)
+post_request = urllib.request.Request("https://kinoregina.fi/wp-content/themes/kinoregina/assets/functions/getShowtimesFrontpage.php", data=post_params)
 
 contents = urllib.request.urlopen(post_request).read().decode('utf-8')
 
